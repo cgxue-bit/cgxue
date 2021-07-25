7ccafef0c32fbc5827a116fde64c5aae7f9c912c
cgxue <cgxue@qq.com>
2021-07-25 12:05:10 +0800

    leecode

---
+++ day01/test1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string name1,name2,resultAll;
+    cout << "put in the first name:";
+    getline(cin, name1);
+    cout << "put in the second name:";
+    getline(cin, name2);
+    //resultAll = name1+name2;
+    cout<<"¹§Ï² " << name1 << " and " << name2 << " Ï²½áÁ¼Ôµ!!!" << endl;
+    system("pause");
+    return 0;
+}
